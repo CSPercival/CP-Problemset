26152c5a4b199a4bddfa94daa5d6148028fe43ae
Cyryl Szatan <cszatan@LAPTOP-OMEN>
2024-08-21 23:10:13 +0200

    solve task 1000. from Timus

---
+++ Timus/1000_A+B_Problem.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define BOOST cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(false)
+
+using namespace std;
+
+int main(){
+    BOOST;
+    int a, b; cin >> a >> b;
+    cout << a + b << "\n";
+    return 0;
+}
