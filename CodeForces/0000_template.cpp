8683f5494a44bab1a3e689bf981223614399b1a9
Cyryl Szatan <cszatan@LAPTOP-OMEN>
2024-08-22 13:14:37 +0200

    add CodeForces folder with template for future solutions

---
+++ CodeForces/0000_template.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define BOOST cin.tie(0);ios_base::sync_with_stdio(false)
+#define st first
+#define nd second
+
+using namespace std;
+
+void solve(){
+
+}
+
+int main(){
+    BOOST;
+    int qq = 1; cin >> qq;
+    while(qq--){
+        solve();
+    }
+}
